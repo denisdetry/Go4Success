ddae15cf1b7a5c661f618d07ae783d81d2c0e67d
Nequow <74051411+Nequow@users.noreply.github.com>
2024-02-27 14:39:00 +0100

    Delete Test react native directory

--- Test react native/django/backend/go4success/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class Go4SuccessSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Go4Success
-        fields = ('id', 'name', 'email', 'phone', 'message')
-        

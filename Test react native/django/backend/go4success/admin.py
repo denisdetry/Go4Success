848c9cb9b8f97cfe54cd97ebec156414f7e2a71f
Nequow <74051411+Nequow@users.noreply.github.com>
2024-02-27 14:44:53 +0100

    Delete Test react native directory

--- Test react native/django/backend/go4success/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-class Go4SuccessAdmin(admin.ModelAdmin):
-    list_display = ("name", "email", "phone", "message")
-    
-    
-admin.site.register(Go4Success, Go4SuccessAdmin)
--- Test react native/django/backend/go4success/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Go4SuccessConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "go4success"

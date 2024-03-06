89c6c98fd40453d86048a7caafb2bb27400ebc6f
artakbukushyan <artaknorair1999@hotmail.com>
2024-03-06 09:20:29 +0100

    update

--- apps/frontend/go4success/components/Card.tsx
+++ apps/frontend/go4success/components/Card.tsx
@@ -126,7 +126,7 @@ const Card: React.FC<CardProps> = ({
             .get("http://localhost:8000/api/current_user/")
             .then((res) => {
                 setCurrentUserID(res.data.user.id);
-                alert(res.data.user.id);
+                console.log("userID : ", res.data.user.id);
             })
             .catch((err) => {
                 console.log(err);

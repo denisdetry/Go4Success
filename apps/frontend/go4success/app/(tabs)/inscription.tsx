b9e230f6ae9300df8503272b22d2fd7c39037af8
djedje-cyber <gerrylongfils@gmail.com>
2024-03-07 18:33:59 +0100

    feat(Create-inscription): create inscription skeleton

---
+++ apps/frontend/go4success/app/(tabs)/inscription.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View,TextInput} from "@/components/Themed";
+
+export default function inscription() {
+    return (
+
+    );
+}
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: "80%",
+    },
+});

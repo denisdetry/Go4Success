af8d91c49ae338f416371b63233fa9d19f5517f4
djedje-cyber <gerrylongfils@gmail.com>
2024-03-11 21:20:19 +0100

    feat(views-and-serializers): create serializer

--- apps/backend/workshop/serializers.py
+++ apps/backend/workshop/serializers.py
@@ -1,2 +1,21 @@
-from django.contrib.auth.models import *
+from api.models import *
 from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=User
+        fields = ['id','username','email','is_staff','is_active']
+
+class RoomSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ROOM
+        fields =  [ 'site_name','room_name']
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = COURSE
+        fields = ['course_code']
+class ActivitySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ACTIVITY
+        fields = '__all__'
--- apps/backend/workshop/views.py
+++ apps/backend/workshop/views.py
@@ -1,5 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-def addworkshop(request):
-    return HttpResponse('')
+
+from rest_framework import permissions, viewsets
+
+from serializers import *
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class RoomViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = ROOM.objects.all()
+    serializer_class = RoomSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = COURSE.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = ACTIVITY.objects.all()
+    serializer_class = ActivitySerializer
+    permission_classes = [permissions.IsAuthenticated]    

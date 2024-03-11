88991437def353919211ea669b0b34264363eb69
Maxou <max.allemeersch@gmail.com>
2024-03-11 19:29:21 +0100

    fix(serializers): fix local display

--- apps/backend/api/serializers.py
+++ apps/backend/api/serializers.py
@@ -39,10 +39,17 @@ class registerToActivitySerializer(serializers.ModelSerializer):
 
 
 class ActivitySerializer(serializers.ModelSerializer):
+    # Utilisation d'un SerializerMethodField pour personnaliser la représentation du champ activity_room
+    activity_room = serializers.SerializerMethodField()
+
     class Meta:
         model = ACTIVITY
         fields = ('activity_id', 'activity_type', 'activity_name', 'activity_description',
                   'activity_date_start', 'activity_date_end', 'activity_room', 'activty_course_code')
+
+    # Méthode pour personnaliser la représentation du champ activity_room
+    def get_activity_room(self, obj):
+        return f"{obj.activity_room.site_name} - {obj.activity_room.room_name}"
 
 
 class AttendsSerializer(serializers.ModelSerializer):

8608489dd54ff390a39cac50b43f4322551f3def
Nequow <74051411+Nequow@users.noreply.github.com>
2024-03-13 22:23:24 +0100

    Update serializers.py

--- apps/backend/api/serializers.py
+++ apps/backend/api/serializers.py
@@ -4,7 +4,26 @@ from django.contrib.auth import authenticate
 from rest_framework.exceptions import ValidationError
 from .models import *
 
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
+    def create(self, clean_data):
+        user_obj = User.objects.create_user(**clean_data)
+        return user_obj
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def check_user(self, clean_data):
+        user = authenticate(**clean_data)
+        if not user:
+            raise ValidationError("User not found")
+        return user
+        
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User

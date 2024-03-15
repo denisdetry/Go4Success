03d9a762d5c320dc3bd2feb2ca1033a9de0bfb1f
Xantin Préser <xantin.preser@gmail.com>
2024-03-15 12:37:46 +0100

    Adding workshop, routing pages, and better requests

--- apps/frontend/go4success/app/_layout.tsx
+++ apps/frontend/go4success/app/_layout.tsx
@@ -1,6 +1,40 @@
 import React from "react";
-import Drawer from "expo-router/drawer";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from "expo-router/drawer";
 
 export default function Layout() {
-    return <Drawer />;
+    return (
+        <GestureHandlerRootView style={{flex: 1}}>
+            <Drawer>
+                <Drawer.Screen
+                    name="index"
+                    options={
+                        {
+                            drawerLabel: "Home",
+                            title: "Home",
+                        }
+                    }
+                />
+                <Drawer.Screen
+                    name="workshops"
+                    options={
+                        {
+                            drawerLabel: "Workshops",
+                            title: "Workshops",
+                        }
+                    }
+                />
+                <Drawer.Screen
+                    name="Page"
+                    options={
+                        {
+                            drawerLabel: "Profile",
+                            title: "Profile",
+                            drawerItemStyle: { display: "none" },
+                        }
+                    }
+                />
+            </Drawer>
+        </GestureHandlerRootView>
+    )
 }
---
+++ apps/frontend/go4success/app/workshops/_layout.tsx
@@ -0,0 +1,6 @@
+import { Navigator } from "expo-router";
+import Slot = Navigator.Slot;
+
+export default function WorkshopsLayout() {
+    return <Slot />;
+}

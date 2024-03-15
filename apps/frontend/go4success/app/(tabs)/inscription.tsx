51bc6a1093c4993dc398019c854d3ab1abeb2603
djedje-cyber <gerrylongfils@gmail.com>
2024-03-15 12:15:16 +0100

    feat(inscription.tsx): create axios request

--- apps/frontend/go4success/app/(tabs)/inscription.tsx
+++ apps/frontend/go4success/app/(tabs)/inscription.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React,{useState} from "react";
 import {
     View,
     Text,
@@ -9,21 +8,42 @@ import {
     StyleSheet,
 } from "react-native";
 
+import axios from "axios";
 export default function inscription() {
+    const [name,setName] = useState('');
+    const [lastname,setLastName] = useState('');
+    const [mail,setMail] = useState('');
+    const [password,setPassword] = useState('');
     return (
         <View>
             <Text>Nom : </Text>
-            <TextInput defaultValue="" />
+            <TextInput defaultValue={name} />
 
             <Text>Prénom : </Text>
-            <TextInput defaultValue="" />
+            <TextInput defaultValue={lastname} />
 
             <Text>Adresse mail :</Text>
-            <TextInput defaultValue="" />
+            <TextInput defaultValue={mail} />
             <Text>Mot de passe :</Text>
-            <TextInput secureTextEntry={true} />
+            <TextInput secureTextEntry={true} defaultValue={password}/>
         </View>
+
+        
     );
+        axios.post('127.0.0.1:8000/registration', {
+            name: name,
+            lastName: lastname,
+            mail :mail,
+            password : password
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+    
 }
 
 const styles = StyleSheet.create({});

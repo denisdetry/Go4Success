d9c76218e86677a330c4a43e62c59c454eea4e81
djedje-cyber <gerrylongfils@gmail.com>
2024-03-21 14:42:08 +0100

    feat(rolemanagment.txs): create the tab

--- apps/frontend/go4success/app/(app)/rolemanagement.tsx
+++ apps/frontend/go4success/app/(app)/rolemanagement.tsx
@@ -1,13 +1,37 @@
-import {View,Text} from 'react-native';
+import {View,Text,FlatList,Picker} from 'react-native';
 import React from "react";
 import styles from "@/styles/global";
 
 
-export default function profile() {
+
+export default function roleManagement (){
+    
+
+  const data = [
+    { id: 1, nom: 'Jean Dupond', valeur: 'Admin' },
+    { id: 2, nom: 'Tintin', valeur: 'Etudiant' },
+    { id: 3, nom: 'Beta', valeur: 'Tuteur' },
+    // Ajoutez autant d'éléments que nécessaire
+  ];
+
+
+
+
+      const renderItem = ({ item }) => (
+    <View style={{ flexDirection: 'row', padding: 10 }}>
+      <Text style={{ flex: 1 }}>{item.nom}</Text>
+      <Text style={{ flex: 1 }}>{item.valeur}</Text>
+    </View>
+  );
+    
+    
     return (
-        <View style={[styles.mainContainer, { justifyContent: "center" }]}>
-            <Text style={styles.title}>gestion des roles</Text>
-        </View>
+  <FlatList
+      data={data}
+      renderItem={renderItem}
+      keyExtractor={item => item.id.toString()}
+      // Vous pouvez également ajouter d'autres props FlatList ici selon vos besoins
+    />
     );
 }
 

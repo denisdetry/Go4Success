3b76c03bf39ef5513c4217ad3dbcf8a3c528aaa0
djedje-cyber <gerrylongfils@gmail.com>
2024-04-03 15:27:10 +0200

    test(test.py): write test for role management

--- apps/backend/rolemanagment/tests.py
+++ apps/backend/rolemanagment/tests.py
@@ -4,6 +4,7 @@ from api.models import Teacher, User
 from .serializers import *
 from rest_framework import *
 from .views import *
+from django.core.exceptions import ValidationError
 
 
 class RoleManagementTest(APITestCase):
@@ -38,7 +39,9 @@ class RoleManagementTest(APITestCase):
         self.user = User.objects.get(username='Le G')
         self.superUser = User.objects.get(username='superUser')
         self.view = UserView.as_view({'get': 'list'})
-
+        self.view_edit = EditRoleView.as_view(
+            {'post': 'create'})
+        self.view_patch = EditRoleView.as_view({'patch': 'partial_update'})
         request = self.factory.get('/api/rolemanagement')
         force_authenticate(request, user=self.user)
 
@@ -56,10 +59,82 @@ class RoleManagementTest(APITestCase):
         response = self.view(request)
         self.assertEqual(dict(response.data[0]), correct_answer)
 
-    def test_isTutor(self):
+    def test_is_tutor(self):
 
         request = self.factory.post(
-            '/api/editrole', {'user': 1, 'is_professor': False, 'is_tutor': True})
+            '/api/editRole/', {'user': 1, 'is_professor': False, 'is_tutor': True}, format='json')
         force_authenticate(request, user=self.superUser)
-        response = self.view(request)
+        response = self.view_edit(request)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_is_professor(self):
+
+        request = self.factory.post(
+            '/api/editRole/', {'user': 1, 'is_professor': True, 'is_tutor': False}, format='json')
+        force_authenticate(request, user=self.superUser)
+        response = self.view_edit(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_cant_be_professor_and_tutor(self):
+        request = self.factory.post(
+            '/api/editRole/', {'user': 1, 'is_professor': True, 'is_tutor': True}, format='json')
+        force_authenticate(request, user=self.superUser)
+        with self.assertRaises(ValidationError) as context:
+            response = self.view_edit(request)
+
+        expected_error_message = '["A Teacher can\'t be a tutor and a professor at the same time!"]'
+        self.assertEqual(str(context.exception), expected_error_message)
+
+    def test_must_have_responsibilities(self):
+
+        request = self.factory.post(
+            '/api/editRole/', {'user': 1, 'is_professor': False, 'is_tutor': False}, format='json')
+        force_authenticate(request, user=self.superUser)
+        with self.assertRaises(ValidationError) as context:
+            response = self.view_edit(request)
+
+        expected_error_message = "['A Teacher has to be a tutor or a professor!']"
+        self.assertEqual(str(context.exception), expected_error_message)
+
+    def test_user_does_not_exists(self):
+
+        request = self.factory.post(
+            '/api/editRole/', {'user': 40, 'is_professor': False, 'is_tutor': False}, format='json')
+        force_authenticate(request, user=self.superUser)
+
+        response = self.view_edit(request)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_patch_user(self):
+
+        user_id = 1
+
+        request_post = self.factory.post(
+            '/api/editRole/', {'user': 1, 'is_professor': False, 'is_tutor': True}, format='json')
+
+        request_patch = self.factory.patch(
+            f'/api/editRole/{user_id}', {'user': 1, 'is_professor': True, 'is_tutor': False}, format='json')
+
+        force_authenticate(request_post, user=self.superUser)
+
+        self.view_edit(request_post)
+        response = self.view_patch(request_patch, pk=user_id)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_patch_professor_to_tutor(self):
+        user_id = 1
+
+        request_post = self.factory.post(
+            '/api/editRole/', {'user': 1, 'is_professor': True, 'is_tutor': False}, format='json')
+
+        request_patch = self.factory.patch(
+            f'/api/editRole/{user_id}', {'user': 1, 'is_professor': False, 'is_tutor': True}, format='json')
+
+        force_authenticate(request_post, user=self.superUser)
+
+        self.view_edit(request_post)
+        response = self.view_patch(request_patch, pk=user_id)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)

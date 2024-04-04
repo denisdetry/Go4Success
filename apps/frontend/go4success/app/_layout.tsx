987cb7b35ec15b7254a50ae9ca946ebfe69453b4
Xantin Préser <xantin.preser@gmail.com>
2024-04-04 02:00:45 +0200

    package install + fixes (Typo, spacing)

--- apps/frontend/go4success/app/_layout.tsx
+++ apps/frontend/go4success/app/_layout.tsx
@@ -3,8 +3,10 @@ import { Slot } from "expo-router";
 import { RootSiblingParent } from "react-native-root-siblings";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../locales/i18n";
+
 export default function Layout() {
     const queryClient = new QueryClient();
+
     return (
         <QueryClientProvider client={queryClient}>
             <AuthProvider>

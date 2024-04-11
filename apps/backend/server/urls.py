84131a4eeeb311db43d64cd0486bdb58e18009b9
djedje-cyber <gerrylongfils@gmail.com>
2024-04-11 22:38:11 +0200

    feat(urls.py): create app skeleton

--- apps/backend/server/urls.py
+++ apps/backend/server/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("auth/", include("authentification.urls")),
     path("activities/", include("activities.urls")),
-    path("rolemanagement/", include("rolemanagment.urls"))
+    path("rolemanagement/", include("rolemanagment.urls")),
+    path("createquestion/", include("createfeedbackquestionnaire.urls"))
 ]

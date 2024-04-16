5a722a8364b2f2ad9230f9b7aec90e41c9693c7a
denisdetry <denis.detry@gmail.com>
2024-04-16 18:10:50 +0200

    Ajout images flèches et timeline

--- apps/frontend/go4success/app/(app)/calendar.tsx
+++ apps/frontend/go4success/app/(app)/calendar.tsx
@@ -1,73 +1,157 @@
-import React from "react";
-import { Alert, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import groupBy from "lodash/groupBy";
+import filter from "lodash/filter";
+import find from "lodash/find";
+
+import React, { Component } from "react";
+import { Alert } from "react-native";
 import {
-    Agenda,
-    DateData,
-    AgendaEntry,
-    AgendaSchedule,
+    ExpandableCalendar,
     TimelineEventProps,
+    TimelineList,
+    CalendarProvider,
+    TimelineProps,
     CalendarUtils,
 } from "react-native-calendars";
-import {
-    useQuery,
-    useMutation,
-    useQueryClient,
-    QueryClient,
-    QueryClientProvider,
-} from "@tanstack/react-query";
-import { Component } from "react";
-import groupBy from "lodash/groupBy";
-import axios from "axios";
-
-interface State {
-    items?: AgendaSchedule;
-}
+import { useQuery } from "@tanstack/react-query";
 
 const EVENT_COLOR = "#e6add8";
 const today = new Date();
-
 export const getDate = (offset = 0) =>
     CalendarUtils.getCalendarDateString(new Date().setDate(today.getDate() + offset));
 
 export const timelineEvents: TimelineEventProps[] = [
     {
+        start: `${getDate(-1)} 09:20:00`,
+        end: `${getDate(-1)} 12:00:00`,
+        title: "Merge Request to React Native Calendars",
+        summary: "Merge Timeline Calendar to React Native Calendars",
+    },
+    {
         start: `${getDate()} 01:15:00`,
         end: `${getDate()} 02:30:00`,
-        title: "Atelier Blocus",
-        summary: "Comment préparer votre blocus ?",
+        title: "Meeting A",
+        summary: "Summary for meeting A",
         color: EVENT_COLOR,
     },
     {
-        start: `${getDate()} 02:30:00`,
-        end: `${getDate()} 03:30:00`,
-        title: "Atelier Gestion du temps",
-        summary: "Comment gérer son temps pendant le quadri ?",
+        start: `${getDate()} 01:30:00`,
+        end: `${getDate()} 02:30:00`,
+        title: "Meeting B",
+        summary: "Summary for meeting B",
         color: EVENT_COLOR,
     },
     {
         start: `${getDate()} 01:45:00`,
         end: `${getDate()} 02:45:00`,
-        title: "Atelier Gestion du stress",
-        summary: "Comment gérer son stress pendant le blocus ?",
+        title: "Meeting C",
+        summary: "Summary for meeting C",
         color: EVENT_COLOR,
     },
+    {
+        start: `${getDate()} 02:40:00`,
+        end: `${getDate()} 03:10:00`,
+        title: "Meeting D",
+        summary: "Summary for meeting D",
+        color: EVENT_COLOR,
+    },
+    {
+        start: `${getDate()} 02:50:00`,
+        end: `${getDate()} 03:20:00`,
+        title: "Meeting E",
+        summary: "Summary for meeting E",
+        color: EVENT_COLOR,
+    },
+    {
+        start: `${getDate()} 04:30:00`,
+        end: `${getDate()} 05:30:00`,
+        title: "Meeting F",
+        summary: "Summary for meeting F",
+        color: EVENT_COLOR,
+    },
+    {
+        start: `${getDate(1)} 00:30:00`,
+        end: `${getDate(1)} 01:30:00`,
+        title: "Visit Grand Mother",
+        summary: "Visit Grand Mother and bring some fruits.",
+        color: "lightblue",
+    },
+    {
+        start: `${getDate(1)} 02:30:00`,
+        end: `${getDate(1)} 03:20:00`,
+        title: "Meeting with Prof. Behjet Zuhaira",
+        summary: "Meeting with Prof. Behjet at 130 in her office.",
+        color: EVENT_COLOR,
+    },
+    {
+        start: `${getDate(1)} 04:10:00`,
+        end: `${getDate(1)} 04:40:00`,
+        title: "Tea Time with Dr. Hasan",
+        summary: "Tea Time with Dr. Hasan, Talk about Project",
+    },
+    {
+        start: `${getDate(1)} 01:05:00`,
+        end: `${getDate(1)} 01:35:00`,
+        title: "Dr. Mariana Joseph",
+        summary: "3412 Piedmont Rd NE, GA 3032",
+    },
+    {
+        start: `${getDate(1)} 14:30:00`,
+        end: `${getDate(1)} 16:30:00`,
+        title: "Meeting Some Friends in ARMED",
+        summary: "Arsalan, Hasnaat, Talha, Waleed, Bilal",
+        color: "pink",
+    },
+    {
+        start: `${getDate(2)} 01:40:00`,
+        end: `${getDate(2)} 02:25:00`,
+        title: "Meet Sir Khurram Iqbal",
+        summary: "Computer Science Dept. Comsats Islamabad",
+        color: "orange",
+    },
+    {
+        start: `${getDate(2)} 04:10:00`,
+        end: `${getDate(2)} 04:40:00`,
+        title: "Tea Time with Colleagues",
+        summary: "WeRplay",
+    },
+    {
+        start: `${getDate(2)} 00:45:00`,
+        end: `${getDate(2)} 01:45:00`,
+        title: "Lets Play Apex Legends",
+        summary: "with Boys at Work",
+    },
+    {
+        start: `${getDate(2)} 11:30:00`,
+        end: `${getDate(2)} 12:30:00`,
+        title: "Dr. Mariana Joseph",
+        summary: "3412 Piedmont Rd NE, GA 3032",
+    },
+    {
+        start: `${getDate(4)} 12:10:00`,
+        end: `${getDate(4)} 13:45:00`,
+        title: "Merge Request to React Native Calendars",
+        summary: "Merge Timeline Calendar to React Native Calendars",
+    },
 ];
-const queryClient = new QueryClient();
 
 const INITIAL_TIME = { hour: 9, minutes: 0 };
 const EVENTS: TimelineEventProps[] = timelineEvents;
 
-export default function AgendaPage() {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <AgendaScreen />
-        </QueryClientProvider>
-    );
+function getAttendance() {
+    console.log("getAttendance called");
+    const attendance = useQuery({
+        queryKey: ["attendance"],
+        queryFn: async () => {
+            const response = await fetch("http://localhost:8081/api/attends");
+            console.log("getAttendance response: ", response);
+            return response.json();
+        },
+    });
 }
 
-/* class TimelineCalendarScreen extends Component {
+export default class TimelineCalendarScreen extends Component {
     state = {
-        currentDate: new Date(),
+        currentDate: getDate(),
         events: EVENTS,
         eventsByDate: groupBy(EVENTS, (e) =>
             CalendarUtils.getCalendarDateString(e.start),
@@ -76,31 +160,138 @@ export default function AgendaPage() {
         },
     };
 
-    render() {
-         const activities = useQuery({
-            queryKey: ["agendaActivitiesQuery"],
-            queryFn: async () => {
-                const res = await axios.get("http://localhost:8000/api/attends/");
-                console.log(res.data);
-                return res.data;
+    marked = {
+        [`${getDate(-1)}`]: { marked: true },
+        [`${getDate()}`]: { marked: true },
+        [`${getDate(1)}`]: { marked: true },
+        [`${getDate(2)}`]: { marked: true },
+        [`${getDate(4)}`]: { marked: true },
+    };
+
+    onDateChanged = (date: string, source: string) => {
+        console.log("TimelineCalendarScreen onDateChanged: ", date, source);
+        this.setState({ currentDate: date });
+    };
+
+    onMonthChange = (month: any, updateSource: any) => {
+        console.log("TimelineCalendarScreen onMonthChange: ", month, updateSource);
+    };
+
+    createNewEvent: TimelineProps["onBackgroundLongPress"] = (
+        timeString,
+        timeObject,
+    ) => {
+        const { eventsByDate } = this.state;
+        const hourString = `${(timeObject.hour + 1).toString().padStart(2, "0")}`;
+        const minutesString = `${timeObject.minutes.toString().padStart(2, "0")}`;
+
+        const newEvent = {
+            id: "draft",
+            start: `${timeString}`,
+            end: `${timeObject.date} ${hourString}:${minutesString}:00`,
+            title: "New Event",
+            color: "white",
+        };
+
+        if (timeObject.date) {
+            if (eventsByDate[timeObject.date]) {
+                eventsByDate[timeObject.date] = [
+                    ...eventsByDate[timeObject.date],
+                    newEvent,
+                ];
+                this.setState({ eventsByDate });
+            } else {
+                eventsByDate[timeObject.date] = [newEvent];
+                this.setState({ eventsByDate: { ...eventsByDate } });
+            }
+        }
+    };
+
+    approveNewEvent: TimelineProps["onBackgroundLongPressOut"] = (
+        _timeString,
+        timeObject,
+    ) => {
+        const { eventsByDate } = this.state;
+
+        Alert.prompt("New Event", "Enter event title", [
+            {
+                text: "Cancel",
+                onPress: () => {
+                    if (timeObject.date) {
+                        eventsByDate[timeObject.date] = filter(
+                            eventsByDate[timeObject.date],
+                            (e) => e.id !== "draft",
+                        );
+
+                        this.setState({
+                            eventsByDate,
+                        });
+                    }
+                },
             },
-        }); 
+            {
+                text: "Create",
+                onPress: (eventTitle) => {
+                    if (timeObject.date) {
+                        const draftEvent = find(eventsByDate[timeObject.date], {
+                            id: "draft",
+                        });
+                        if (draftEvent) {
+                            draftEvent.id = undefined;
+                            draftEvent.title = eventTitle ?? "New Event";
+                            draftEvent.color = "lightgreen";
+                            eventsByDate[timeObject.date] = [
+                                ...eventsByDate[timeObject.date],
+                            ];
+
+                            this.setState({
+                                eventsByDate,
+                            });
+                        }
+                    }
+                },
+            },
+        ]);
+    };
+
+    private timelineProps: Partial<TimelineProps> = {
+        format24h: true,
+        onBackgroundLongPress: this.createNewEvent,
+        onBackgroundLongPressOut: this.approveNewEvent,
+        // scrollToFirst: true,
+        // start: 0,
+        // end: 24,
+        unavailableHours: [
+            { start: 0, end: 6 },
+            { start: 22, end: 24 },
+        ],
+        overlapEventsSpacing: 8,
+        rightEdgeSpacing: 24,
+    };
+
+    format24h = true;
+
+    render() {
+        const { currentDate, eventsByDate } = this.state;
+
         return (
             <CalendarProvider
-                date={Date()}
+                date={currentDate}
+                onDateChanged={this.onDateChanged}
+                onMonthChange={this.onMonthChange}
                 showTodayButton
                 disabledOpacity={0.6}
                 // numberOfDays={3}
             >
                 <ExpandableCalendar
                     firstDay={1}
-                    //leftArrowImageSource={require("../img/previous.png")}
-                    //rightArrowImageSource={require("../img/next.png")}
-                    //markedDates={this.marked}
-                    initialPosition={ExpandableCalendar.positions.CLOSED}
+                    leftArrowImageSource={require("../../assets/images/previous.png")}
+                    rightArrowImageSource={require("../../assets/images/next.png")}
+                    markedDates={this.marked}
                 />
                 <TimelineList
-                    events={this.state.eventsByDate}
+                    events={eventsByDate}
+                    timelineProps={this.timelineProps}
                     showNowIndicator
                     // scrollToNow
                     scrollToFirst
@@ -110,80 +301,3 @@ export default function AgendaPage() {
         );
     }
 }
- */
-export class AgendaScreen extends Component<State> {
-    render() {
-        return (
-            <Agenda
-                items={{
-                    "2021-10-01": [
-                        { name: "item 1 - any js object", height: 100, day: "" },
-                    ],
-                }}
-                selected={"2021-10-01"}
-                renderItem={this.renderItem}
-                renderDay={this.renderDay}
-                renderEmptyDate={this.renderEmptyDate}
-                showClosingKnob={true}
-                hideKnob={false}
-                style={styles.Agenda}
-            />
-        );
-    }
-
-    renderEmptyDate = () => {
-        return (
-            <View style={styles.emptyDate}>
-                <Text>This is empty date!</Text>
-            </View>
-        );
-    };
-
-    renderItem = (reservation: AgendaEntry, isFirst: boolean) => {
-        const fontSize = isFirst ? 16 : 14;
-        const color = isFirst ? "black" : "#43515c";
-
-        return (
-            <TouchableOpacity
-                style={[styles.item, { height: reservation.height }]}
-                onPress={() => Alert.alert(reservation.name)}
-            >
-                <Text style={{ fontSize, color }}>{reservation.name}</Text>
-            </TouchableOpacity>
-        );
-    };
-
-    renderDay = (day: Date) => {
-        if (day) {
-            return <Text style={styles.customDay}>{day.getDay()}</Text>;
-        }
-        return <View style={styles.dayItem} />;
-    };
-}
-
-const styles = StyleSheet.create({
-    item: {
-        backgroundColor: "white",
-        flex: 1,
-        borderRadius: 5,
-        padding: 10,
-        marginRight: 10,
-        marginTop: 17,
-    },
-    emptyDate: {
-        height: 15,
-        flex: 1,
-        paddingTop: 30,
-    },
-    customDay: {
-        margin: 10,
-        fontSize: 24,
-        color: "green",
-    },
-    dayItem: {
-        marginLeft: 34,
-    },
-    Agenda: {
-        maxWidth: 400,
-    },
-});

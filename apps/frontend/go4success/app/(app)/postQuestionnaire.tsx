6ce2af987bec6d907ecd6935975602b3f02fb80d
djedje-cyber <gerrylongfils@gmail.com>
2024-04-22 21:09:14 +0200

    feat(postQuestionnaire.tsx): create canva

---
+++ apps/frontend/go4success/app/(app)/postQuestionnaire.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import {
+    Picker,
+    FlatList,
+    Text,
+    TouchableOpacity,
+    View,
+    StyleSheet,
+} from "react-native";
+import { useQuestionnaire } from "@/hooks/useQuestionnaire";
+
+export default function RoleManagement() {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    );
+}

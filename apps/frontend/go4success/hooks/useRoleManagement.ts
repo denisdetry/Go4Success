59ff8b66f06d1ee787dd1e7cd604820fb72680b4
djedje-cyber <gerrylongfils@gmail.com>
2024-04-29 19:18:14 +0200

    fix(useRolemanagement.tsx): write user role

--- apps/frontend/go4success/hooks/useRoleManagement.ts
+++ apps/frontend/go4success/hooks/useRoleManagement.ts
@@ -15,19 +15,38 @@ interface UserRole {
 }
 
 export function useRoleManagement(
-    id: string,
+    id: number | null,
     first_name: string | undefined,
     last_name: string | undefined,
     is_superUser: string | undefined,
     is_professor: string | null,
     is_tutor: string | null,
+    user: number | null,
 ) {
     const backend_url = process.env.EXPO_PUBLIC_API_URL;
 
     // console.log("Called useActivities");
 
+    const { isPending, data, error } = useQuery<UserRole[]>({
+        queryKey: ["userRole", user, is_professor, is_tutor],
+        queryFn: async () => {
+            const { data } = await fetchBackend({
+                type: "GET",
+                url: `/rolemanagement/${endpoint}/`,
+                params: {
+                    user: user,
+                    // eslint-disable-next-line camelcase
+                    is_professor: is_professor,
+                    // eslint-disable-next-line camelcase
+                    is_tutor: is_tutor,
+                },
+            });
+            // console.log("resp:", data);
+            return data;
+        },
+    });
     const { isPending, data, error } = useQuery<User[]>({
-        queryKey: ["rolemanagement", id, first_name, last_name, is_superuser],
+        queryKey: ["rolemanagement", id, first_name, last_name, is_superUser],
         queryFn: async () => {
             const { data } = await fetchBackend({
                 type: "GET",

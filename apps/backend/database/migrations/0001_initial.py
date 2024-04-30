6113a737a6592e26f3a807bd8aafa132e6dd0c93
Nequow <74051411+Nequow@users.noreply.github.com>
2024-04-30 14:00:42 +0200

    Gitignore for migrations folders (#42)
    * add gitignore for migrations folders
    remove migrations folders
    
    * add gitignore for migrations folders
    remove migrations folders

--- apps/backend/database/migrations/0001_initial.py
+++ apps/backend/database/migrations/0001_initial.py
@@ -18,20 +18,27 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='User',
             fields=[
-                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('password', models.CharField(
+                    max_length=128, verbose_name='password')),
                 ('id', models.AutoField(primary_key=True, serialize=False)),
-                ('username', models.CharField(error_messages={'unique': "Ce nom d'utilisateur est déjà utilisé."}, max_length=255, unique=True)),
-                ('email', models.EmailField(error_messages={'unique': 'Cette adresse mail est déjà utilisée.'}, max_length=254, unique=True)),
+                ('username', models.CharField(error_messages={
+                 'unique': "Ce nom d'utilisateur est déjà utilisé."}, max_length=255, unique=True)),
+                ('email', models.EmailField(error_messages={
+                 'unique': 'Cette adresse mail est déjà utilisée.'}, max_length=254, unique=True)),
                 ('first_name', models.CharField(max_length=255)),
                 ('last_name', models.CharField(max_length=255)),
-                ('noma', models.CharField(blank=True, error_messages={'unique': 'Ce noma est déjà utilisé.'}, max_length=63, null=True, unique=True)),
+                ('noma', models.CharField(blank=True, error_messages={
+                 'unique': 'Ce noma est déjà utilisé.'}, max_length=63, null=True, unique=True)),
                 ('is_active', models.BooleanField(default=True)),
                 ('is_staff', models.BooleanField(default=False)),
                 ('is_superuser', models.BooleanField(default=False)),
-                ('date_join', models.DateTimeField(default=django.utils.timezone.now)),
+                ('date_join', models.DateTimeField(
+                    default=django.utils.timezone.now)),
                 ('last_login', models.DateTimeField(blank=True, null=True)),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
+                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+                 related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.',
+                 related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
             ],
             options={
                 'verbose_name': 'user',
@@ -43,7 +50,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(primary_key=True, serialize=False)),
                 ('question', models.TextField()),
-                ('type', models.CharField(choices=[('open', 'Open'), ('multiple_choice', 'Multiple Choice')])),
+                ('type', models.CharField(choices=[
+                 ('open', 'Open'), ('multiple_choice', 'Multiple Choice')])),
                 ('points', models.IntegerField()),
             ],
         ),
@@ -64,7 +72,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Teacher',
             fields=[
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE,
+                 primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
                 ('is_tutor', models.BooleanField()),
                 ('is_professor', models.BooleanField()),
             ],
@@ -73,8 +82,10 @@ class Migration(migrations.Migration):
             name='ChoiceAnswer',
             fields=[
                 ('id', models.AutoField(primary_key=True, serialize=False)),
-                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-                ('question', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='database.question')),
+                ('student', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('question', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='database.question')),
             ],
         ),
         migrations.CreateModel(
@@ -83,7 +94,8 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(primary_key=True, serialize=False)),
                 ('choice', models.TextField()),
                 ('is_correct', models.BooleanField()),
-                ('choice_answer', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='database.choiceanswer')),
+                ('choice_answer', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='database.choiceanswer')),
             ],
         ),
         migrations.CreateModel(
@@ -104,8 +116,10 @@ class Migration(migrations.Migration):
                 ('title', models.CharField(max_length=255)),
                 ('description', models.TextField()),
                 ('publication_date', models.DateTimeField()),
-                ('course', models.ForeignKey(blank=True, max_length=63, null=True, on_delete=django.db.models.deletion.CASCADE, to='database.course')),
-                ('teacher', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='database.teacher')),
+                ('course', models.ForeignKey(blank=True, max_length=63, null=True,
+                 on_delete=django.db.models.deletion.CASCADE, to='database.course')),
+                ('teacher', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='database.teacher')),
             ],
         ),
         migrations.CreateModel(
@@ -117,7 +131,8 @@ class Migration(migrations.Migration):
                 ('description', models.TextField()),
                 ('date_start', models.DateTimeField()),
                 ('date_end', models.DateTimeField()),
-                ('course', models.ForeignKey(blank=True, max_length=63, null=True, on_delete=django.db.models.deletion.CASCADE, to='database.course')),
+                ('course', models.ForeignKey(blank=True, max_length=63, null=True,
+                 on_delete=django.db.models.deletion.CASCADE, to='database.course')),
             ],
         ),
         migrations.CreateModel(
@@ -130,8 +145,10 @@ class Migration(migrations.Migration):
                 ('suggestion', models.TextField(blank=True, null=True)),
                 ('additional_comment', models.TextField(blank=True, null=True)),
                 ('date_submitted', models.DateField(auto_now_add=True)),
-                ('activity', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='database.activity')),
-                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('activity', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='database.activity')),
+                ('student', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
@@ -148,7 +165,8 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='activity',
             name='language',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='database.language'),
+            field=models.ForeignKey(
+                blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='database.language'),
         ),
         migrations.CreateModel(
             name='Message',
@@ -156,8 +174,10 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(primary_key=True, serialize=False)),
                 ('content', models.TextField()),
                 ('date', models.DateTimeField()),
-                ('from_user', models.ForeignKey(blank=True, max_length=63, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='from_user', to=settings.AUTH_USER_MODEL)),
-                ('to_user', models.ForeignKey(blank=True, max_length=63, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='to_user', to=settings.AUTH_USER_MODEL)),
+                ('from_user', models.ForeignKey(blank=True, max_length=63, null=True,
+                 on_delete=django.db.models.deletion.CASCADE, related_name='from_user', to=settings.AUTH_USER_MODEL)),
+                ('to_user', models.ForeignKey(blank=True, max_length=63, null=True,
+                 on_delete=django.db.models.deletion.CASCADE, related_name='to_user', to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
@@ -166,8 +186,10 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(primary_key=True, serialize=False)),
                 ('answer', models.TextField()),
                 ('is_correct', models.BooleanField()),
-                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-                ('question', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='database.question')),
+                ('student', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('question', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='database.question')),
             ],
         ),
         migrations.CreateModel(
@@ -179,31 +201,39 @@ class Migration(migrations.Migration):
                 ('points_total', models.IntegerField()),
                 ('date_start', models.DateTimeField()),
                 ('date_end', models.DateTimeField()),
-                ('course', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='database.course')),
-                ('language', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='database.language')),
+                ('course', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='database.course')),
+                ('language', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='database.language')),
             ],
         ),
         migrations.AddField(
             model_name='question',
             name='questionnaire',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='database.questionnaire'),
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE, to='database.questionnaire'),
         ),
         migrations.AddField(
             model_name='activity',
             name='room',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='database.room'),
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE, to='database.room'),
         ),
         migrations.AddField(
             model_name='room',
             name='site',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='database.site'),
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE, to='database.site'),
         ),
         migrations.CreateModel(
             name='Attend',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('activity', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='database.activity')),
-                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
+                ('activity', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='database.activity')),
+                ('student', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'unique_together': {('activity', 'student')},
@@ -212,9 +242,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Registered',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('course', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='database.course')),
-                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
+                ('course', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='database.course')),
+                ('student', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'unique_together': {('student', 'course')},
@@ -223,9 +256,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='See',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('announcement', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='database.announcement')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
+                ('announcement', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='database.announcement')),
+                ('user', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'unique_together': {('announcement', 'user')},
@@ -238,9 +274,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Give',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('activity', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='database.activity')),
-                ('teacher', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='database.teacher')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
+                ('activity', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='database.activity')),
+                ('teacher', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='database.teacher')),
             ],
             options={
                 'unique_together': {('activity', 'teacher')},

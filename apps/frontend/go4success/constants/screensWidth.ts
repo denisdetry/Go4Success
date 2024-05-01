035177e23c533c0b65490fd4fe8407470f9906a0
artakbukushyan <artaknorair1999@hotmail.com>
2024-05-01 22:43:00 +0200

    change device width in screensWidth.ts

--- apps/frontend/go4success/constants/screensWidth.ts
+++ apps/frontend/go4success/constants/screensWidth.ts
@@ -2,5 +2,5 @@ import { Dimensions } from "react-native";
 
 export const width = Dimensions.get("screen").width;
 export const isMobile = width <= 430;
-export const isTabletMini = width > 400 && width <= 600;
+export const isTabletMini = width > 450 && width <= 600;
 export const isTablet = width > 600 && width <= 992;

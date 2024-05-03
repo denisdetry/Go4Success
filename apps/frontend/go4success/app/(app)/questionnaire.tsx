caaccb2fb502c574475fa786486a31351300f7ef
djedje-cyber <gerrylongfils@gmail.com>
2024-05-03 12:01:35 +0200

    feat(questionnaire.tsx): write picker for date picking

--- apps/frontend/go4success/app/(app)/questionnaire.tsx
+++ apps/frontend/go4success/app/(app)/questionnaire.tsx
@@ -10,7 +10,6 @@ import {
     ScrollView,
     ActivityIndicator,
 } from "react-native";
-import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function App() {
     const { isPending, sites, error } = useCourses();
@@ -60,19 +59,163 @@ export default function App() {
             />
             <View style={styles.labelContainer}>
                 <Text style={styles.label}>Start:</Text>
-                <DateTimePicker
-                    value={formData.startDate}
-                    mode="date"
-                    onChange={handleStartDateChange}
-                />
+                <Picker
+                    style={styles.picker}
+                    selectedValue={formData.language}
+                    onValueChange={(itemValue, itemIndex) =>
+                        handleChange("startDay", itemValue)
+                    }
+                >
+                    <Picker.Item label="Select the day" value="" />
+                    <Picker.Item label="1" value="1" />
+                    <Picker.Item label="2" value="2" />
+                    <Picker.Item label="3" value="3" />
+                    <Picker.Item label="4" value="4" />
+                    <Picker.Item label="5" value="5" />
+                    <Picker.Item label="6" value="6" />
+                    <Picker.Item label="7" value="7" />
+                    <Picker.Item label="8" value="8" />
+                    <Picker.Item label="9" value="9" />
+                    <Picker.Item label="10" value="10" />
+                    <Picker.Item label="11" value="11" />
+                    <Picker.Item label="12" value="12" />
+                    <Picker.Item label="13" value="13" />
+                    <Picker.Item label="14" value="14" />
+                    <Picker.Item label="15" value="15" />
+                    <Picker.Item label="16" value="16" />
+                    <Picker.Item label="17" value="17" />
+                    <Picker.Item label="18" value="18" />
+                    <Picker.Item label="19" value="19" />
+                    <Picker.Item label="20" value="20" />
+                    <Picker.Item label="21" value="21" />
+                    <Picker.Item label="22" value="22" />
+                    <Picker.Item label="23" value="23" />
+                    <Picker.Item label="24" value="24" />
+                    <Picker.Item label="25" value="25" />
+                    <Picker.Item label="26" value="26" />
+                    <Picker.Item label="27" value="27" />
+                    <Picker.Item label="28" value="28" />
+                    <Picker.Item label="29" value="29" />
+                    <Picker.Item label="30" value="30" />
+                    <Picker.Item label="31" value="31" />
+                </Picker>
+                <Picker
+                    style={styles.picker}
+                    selectedValue={formData.language}
+                    onValueChange={(itemValue, itemIndex) =>
+                        handleChange("StartMonth", itemValue)
+                    }
+                >
+                    <Picker.Item label="Select the month" value="" />
+                    <Picker.Item label="1" value="1" />
+                    <Picker.Item label="2" value="2" />
+                    <Picker.Item label="3" value="3" />
+                    <Picker.Item label="4" value="4" />
+                    <Picker.Item label="5" value="5" />
+                    <Picker.Item label="6" value="6" />
+                    <Picker.Item label="7" value="7" />
+                    <Picker.Item label="8" value="8" />
+                    <Picker.Item label="9" value="9" />
+                    <Picker.Item label="10" value="10" />
+                    <Picker.Item label="11" value="11" />
+                    <Picker.Item label="12" value="12" />
+                </Picker>
+                <Picker
+                    style={styles.picker}
+                    selectedValue={formData.language}
+                    onValueChange={(itemValue, itemIndex) =>
+                        handleChange("StartYear", itemValue)
+                    }
+                >
+                    <Picker.Item label="Select the year" value="" />
+                    <Picker.Item label="2024" value="2024" />
+                    <Picker.Item label="2025" value="2025" />
+                    <Picker.Item label="2026" value="2026" />
+                    <Picker.Item label="2027" value="2027" />
+                    <Picker.Item label="2028" value="2028" />
+                    <Picker.Item label="2029" value="2029" />
+                    <Picker.Item label="2030" value="2030" />
+                </Picker>
             </View>
             <View style={styles.labelContainer}>
                 <Text style={styles.label}>End:</Text>
-                <DateTimePicker
-                    value={formData.endDate}
-                    mode="date"
-                    onChange={handleEndDateChange}
-                />
+                <Picker
+                    style={styles.picker}
+                    selectedValue={formData.language}
+                    onValueChange={(itemValue, itemIndex) =>
+                        handleChange("EndDay", itemValue)
+                    }
+                >
+                    <Picker.Item label="Select the day" value="" />
+                    <Picker.Item label="1" value="1" />
+                    <Picker.Item label="2" value="2" />
+                    <Picker.Item label="3" value="3" />
+                    <Picker.Item label="4" value="4" />
+                    <Picker.Item label="5" value="5" />
+                    <Picker.Item label="6" value="6" />
+                    <Picker.Item label="7" value="7" />
+                    <Picker.Item label="8" value="8" />
+                    <Picker.Item label="9" value="9" />
+                    <Picker.Item label="10" value="10" />
+                    <Picker.Item label="11" value="11" />
+                    <Picker.Item label="12" value="12" />
+                    <Picker.Item label="13" value="13" />
+                    <Picker.Item label="14" value="14" />
+                    <Picker.Item label="15" value="15" />
+                    <Picker.Item label="16" value="16" />
+                    <Picker.Item label="17" value="17" />
+                    <Picker.Item label="18" value="18" />
+                    <Picker.Item label="19" value="19" />
+                    <Picker.Item label="20" value="20" />
+                    <Picker.Item label="21" value="21" />
+                    <Picker.Item label="22" value="22" />
+                    <Picker.Item label="23" value="23" />
+                    <Picker.Item label="24" value="24" />
+                    <Picker.Item label="25" value="25" />
+                    <Picker.Item label="26" value="26" />
+                    <Picker.Item label="27" value="27" />
+                    <Picker.Item label="28" value="28" />
+                    <Picker.Item label="29" value="29" />
+                    <Picker.Item label="30" value="30" />
+                    <Picker.Item label="31" value="31" />
+                </Picker>
+                <Picker
+                    style={styles.picker}
+                    selectedValue={formData.language}
+                    onValueChange={(itemValue, itemIndex) =>
+                        handleChange("EndMonth", itemValue)
+                    }
+                >
+                    <Picker.Item label="Select the month" value="" />
+                    <Picker.Item label="1" value="1" />
+                    <Picker.Item label="2" value="2" />
+                    <Picker.Item label="3" value="3" />
+                    <Picker.Item label="4" value="4" />
+                    <Picker.Item label="5" value="5" />
+                    <Picker.Item label="6" value="6" />
+                    <Picker.Item label="7" value="7" />
+                    <Picker.Item label="8" value="8" />
+                    <Picker.Item label="9" value="9" />
+                    <Picker.Item label="10" value="10" />
+                    <Picker.Item label="11" value="11" />
+                    <Picker.Item label="12" value="12" />
+                </Picker>
+                <Picker
+                    style={styles.picker}
+                    selectedValue={formData.language}
+                    onValueChange={(itemValue, itemIndex) =>
+                        handleChange("EndYear", itemValue)
+                    }
+                >
+                    <Picker.Item label="Select the year" value="" />
+                    <Picker.Item label="2024" value="2024" />
+                    <Picker.Item label="2025" value="2025" />
+                    <Picker.Item label="2026" value="2026" />
+                    <Picker.Item label="2027" value="2027" />
+                    <Picker.Item label="2028" value="2028" />
+                    <Picker.Item label="2029" value="2029" />
+                    <Picker.Item label="2030" value="2030" />
+                </Picker>
             </View>
             <Picker
                 style={styles.picker}
@@ -147,6 +290,7 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: "center",
         marginBottom: 15,
+        width: 200,
     },
     label: {
         marginRight: 10,

7dd9a1e91c5f52b6f8992c7930e2882c9795d419
Xantin <56741168+Xiticks@users.noreply.github.com>
2024-05-08 08:46:16 +0200

    Fix/rolemanagement (#68)
    * feat(urls.py): create app skeleton
    
    * feat(postquestionnaire): create backend skeleton for the feature
    
    * feat(serializers.py): create serrializer
    
    * feat(permissions.py): write permissions for professor
    
    * feat(views.py): create questionnaire view
    
    * feat(data.json): add data exemple for question
    
    * feat(views.py): create views for Question
    
    * fix(data.json): fix pk bug
    
    * feat(data.json): write test data for questionnaire
    
    * feat(views.py): write view for posting question
    
    * make a commit
    
    * fix(test.py): fix some test
    
    * feat(complete-views): views.py
    
    * feat(views.py): make views
    
    * feat(postQuestionnaire.tsx): create canva
    
    * feat(viewCourse.tsx): Page for adding questionnaire
    
    * feat(questionnaire.tsx): create hooks and create pages
    
    * feat(questionnaire.tsx): create questionnaire
    
    * feat(question-feature): add css
    
    * fix(questionnaire.tsx): try to fix css
    
    * Gitignore for migrations folders (#42)
    
    * add gitignore for migrations folders
    remove migrations folders
    
    * add gitignore for migrations folders
    remove migrations folders
    
    * feat(questionnaire.tsx): Style questionnaire
    
    * feat(questionnaire.tsx): write picker for date picking
    
    * feat(questionnaire.tsx): add tanstack post
    
    * fix(useQuestionnaire.tsx): fix post query
    
    * fix(useQuestionnaire): try to fix tanstack
    
    * fix(useQuestionnaire): try to fix tanstack
    
    * feat(settings.py): remove is authicated by defaul
    
    * feat(question.tsx): edit question
    
    * feat(question.tsx): create closed question feature
    
    * feat(question.tsx): write save button
    
    * feat(question.tsx): add save button
    
    * feat(viewcourse.tsx): add route
    
    * fix(fix-viewCourse): viewCourses/tsx
    
    * feat(viewcourse.tsx): finish to connect with backend
    
    * feat(postquestionnaire-backend): add open et closed questions view
    
    * feat(questionnaire.tsx): go to question page
    
    * add thingd
    
    * feat(question.tsx): implement com with backend logic for openQuestion
    
    * feat(question.tsx): put a tuple on option
    
    * fix(questions.tsx): bad request fix
    
    * fix(question.tsx): fix send question
    
    * fix(questions.tsx): fix send backend bug
    
    * fix(question.tsx): fix bugs
    
    * test questionnaire
    
    * merge
    
    * add package
    
    * Fixing tests on questionnaire
    
    * fix(question.tsx): fix GET after POST
    
    * Tests questionnaire and doc
    
    * fix(question.tsx): fix undifined question
    
    * fix(question.tsx): finish fixing asynchronous and send option
    
    * fix(question.tsx): fix questionnaire
    
    * feat(question.tsx): add toast
    
    * feat(questionnaire.tsx): hiding from side panel and translation
    
    * feat(questionnaire): finish the feature
    
    * fix(backend): remove migrations and init file
    
    * Delete migration file
    
    * Fixing mistake of deleting __init__.py
    
    * Fixing mistake of deleting __init__.py
    
    * Adding translations
    
    --------

--- apps/backend/rolemanagment/serializers.py
+++ apps/backend/rolemanagment/serializers.py
@@ -1,23 +1,16 @@
-from django.contrib.auth import authenticate
 from database.models import Teacher, User
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name',
-                  'is_superuser']
+        fields = ['id', 'first_name', 'last_name', 'email', 'noma', 'is_staff', 'is_superuser']
 
 
 class TeacherSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Teacher
-        fields = ['is_tutor', 'is_professor']
-
-
-class EditRoleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Teacher
-        fields = ['user', 'is_professor', 'is_tutor']
+        fields = ['user', 'is_tutor', 'is_professor']
